5fa9419dee531c7d3159009d010037aa6a9e2935
Yasuhiro Morioka <ymorioka@gmail.com>
2024-12-16 21:19:10 +0900

    WIP

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import shap
+import plotly.express as px
+import numpy as np
+from scipy.stats import norm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern, DotProduct, WhiteKernel, RBF, ConstantKernel
+
+import random
+
+
+
 def SHAP_explain(BO_model, autoscaled_x, x):
     with st.spinner():
         explainer = shap.KernelExplainer(BO_model.predict, autoscaled_x)
@@ -9,6 +24,110 @@ def SHAP_explain(BO_model, autoscaled_x, x):
     
     return 'shap_summary_plot.png'
 
+def plot_scatter(data, x_col, y_col, z_col, color_col):
+    # Streamlitを使って3D散布図を簡単に表示する
+    # https://zenn.dev/nishijima13/articles/4891b9a05b2854
+    fig = px.scatter_3d(
+        data,
+        x=x_col,
+        y=y_col,
+        z=z_col,
+        color=color_col,
+    )
+    # グラフレイアウト設定
+    #fig.update_layout(title="3D Point Cloud", width=500, height=500)
+    fig.update_layout(width=500, height=500)
+    # マーカーサイズ設定
+    fig.update_traces(marker_size=2)
+    # グラフ表示
+    st.plotly_chart(fig, use_container_width=True)
+
+def sample_generation(limit_data):  # ->generation_sample
+    n_samples = 30
+
+    limits = limit_data.values
+    x = [[random.uniform(lower, upper) for (upper, lower) in limits] for _ in range(n_samples)]
+    return np.array(x)
+
+def BO(data, generation_sample):    # -> next_samples, BO_model, autoscaled_x
+    # https://github.com/hkaneko1985/design_of_experiments/blob/master/Python/bayesianoptimization.py
+    # def bayesianoptimization(X, y, candidates_of_X, acquisition_function_flag, cumulative_variance=None):
+    """
+    Bayesian optimization
+    
+    Gaussian process regression model is constructed between X and y.
+    A candidate of X with the highest acquisition function is selected using the model from candidates of X.
+
+    Parameters
+    ----------
+    X: numpy.array or pandas.DataFrame
+        m x n matrix of X-variables of training dataset (m is the number of samples and n is the number of X-variables)
+    y: numpy.array or pandas.DataFrame
+        m x 1 vector of a y-variable of training dataset
+    candidates_of_X: numpy.array or pandas.DataFrame
+        Candidates of X
+    acquisition_function_flag: int
+        1: Mutual information (MI), 2: Expected improvement(EI), 
+        3: Probability of improvement (PI) [0: Estimated y-values]
+    cumulative_variance: numpy.array or pandas.DataFrame
+        cumulative variance in mutual information (MI)[acquisition_function_flag=1]
+
+    Returns
+    -------
+    selected_candidate_number : int
+        selected number of candidates_of_X
+    selected_X_candidate : numpy.array
+        selected X candidate
+    cumulative_variance: numpy.array
+        cumulative variance in mutual information (MI)[acquisition_function_flag=1]
+    """
+    X = data.iloc[:, 1:]
+    y = data.iloc[:, 0]
+    candidates_of_X = generation_sample
+    acquisition_function_flag = 2
+    cumulative_variance = None
+
+    X = np.array(X)
+    y = np.array(y)
+    if cumulative_variance is None:
+        cumulative_variance = np.empty(len(y))
+    else:
+        cumulative_variance = np.array(cumulative_variance)
+
+    relaxation_value = 0.01
+    delta = 10 ** -6
+    alpha = np.log(2 / delta)
+
+    autoscaled_X = (X - X.mean(axis=0)) / X.std(axis=0, ddof=1)
+    autoscaled_candidates_of_X = (candidates_of_X - X.mean(axis=0)) / X.std(axis=0, ddof=1)
+    autoscaled_y = (y - y.mean(axis=0)) / y.std(axis=0, ddof=1)
+    gaussian_process_model = GaussianProcessRegressor(ConstantKernel() * RBF() + WhiteKernel(), alpha=0)
+    gaussian_process_model.fit(autoscaled_X, autoscaled_y)
+    autoscaled_estimated_y_test, autoscaled_std_of_estimated_y_test = gaussian_process_model.predict(
+        autoscaled_candidates_of_X, return_std=True)
+
+    if acquisition_function_flag == 1:
+        acquisition_function_values = autoscaled_estimated_y_test + alpha ** 0.5 * (
+                (autoscaled_std_of_estimated_y_test ** 2 + cumulative_variance) ** 0.5 - cumulative_variance ** 0.5)
+        cumulative_variance = cumulative_variance + autoscaled_std_of_estimated_y_test ** 2
+    elif acquisition_function_flag == 2:
+        acquisition_function_values = (autoscaled_estimated_y_test - max(autoscaled_y) - relaxation_value) * \
+                                      norm.cdf((autoscaled_estimated_y_test - max(autoscaled_y) - relaxation_value) /
+                                               autoscaled_std_of_estimated_y_test) + \
+                                      autoscaled_std_of_estimated_y_test * \
+                                      norm.pdf((autoscaled_estimated_y_test - max(autoscaled_y) - relaxation_value) /
+                                               autoscaled_std_of_estimated_y_test)
+    elif acquisition_function_flag == 3:
+        acquisition_function_values = norm.cdf(
+            (autoscaled_estimated_y_test - max(autoscaled_y) - relaxation_value) / autoscaled_std_of_estimated_y_test)
+    elif acquisition_function_flag == 0:
+        acquisition_function_values = autoscaled_estimated_y_test
+
+    selected_candidate_number = np.where(acquisition_function_values == max(acquisition_function_values))[0][0]
+    selected_X_candidate = candidates_of_X[selected_candidate_number, :]
+
+    #return selected_candidate_number, selected_X_candidate, cumulative_variance
+    return selected_X_candidate, gaussian_process_model, autoscaled_X
  
 def tabs_set(data, limit_data):
     tab_titles = ["データの確認", "実験点の提案", "解析", "予測"]
@@ -48,6 +167,14 @@ def tabs_set(data, limit_data):
                 ss['x'] = x
 
                 # --------------------省略----------------------------------
+                y_estimated_y_plot = [[0, 1],[0,1]]
+                y_estimated_y_r2 = 0
+                y_estimated_y_mae = 0
+                y_estimated_y_rmse = 0
+                y_estimated_y_in_cv_plot = [[0, 1],[0,1]]
+                y_estimated_y_in_cv_r2 = 0
+                y_estimated_y_in_cv_mae = 0
+                y_estimated_y__in_cv_rmse = 0
 
                 col1, col2 = st.columns(2)
                 
@@ -84,8 +211,12 @@ def tabs_set(data, limit_data):
     with tabs[2]:
         if st.checkbox('SHAP解析', help='「実験点の提案」タブでモデルを構築してから'):
             if 'SHAP_plot' not in ss:
-                # --------------------省略----------------------------------
                 pass
+                # --------------------省略----------------------------------
+                ss['SHAP_plot'] = SHAP_explain(ss['BO_model'],
+                                               ss['autoscaled_x'],
+                                               ss['x'])
+                # で表示
 
 def main():
     st.set_page_config(
